b669e973d07fa56f8674ee7f3e03b4e9942c97c3
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-11-26 16:51:51 +0100

    Base test project initial commit

---
+++ ShopCSharpSeleniumAutomationTests2/BaseTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace ShopCSharpSeleniumAutomationTests2
+{
+    class BaseTest
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void Init()
+        {
+
+        }
+    }
+}
---
+++ ShopCSharpSeleniumAutomationTests2/page/MenuPageTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using ShopCSharpSeleniumAutomation.page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace ShopCSharpSeleniumAutomation.page.Tests
+{
+    [TestFixture()]
+    public class MenuPageTests
+    {
+        [Test()]
+        public void MenuPageTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
