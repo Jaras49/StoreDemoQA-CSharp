f6a8b5f49df350a53502bd175fbe15ed66067113
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-11-29 10:05:43 +0100

    ProductPage impl

--- ShopCSharpSeleniumAutomation/page/category/ProductPage.cs
+++ ShopCSharpSeleniumAutomation/page/category/ProductPage.cs
@@ -1,34 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using log4net;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
 using OpenQA.Selenium.Support.UI;
 
 namespace ShopCSharpSeleniumAutomation.page.category
 {
-    //TODO implement me
     public class ProductPage : WebElementManipulator<ProductPage>
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(ProductPage).Name);
+        private static readonly string removeDashesRegex = "\\p{Pd}";
+
         public MenuPage Menu { get; }
+
+        [FindsBy(How = How.CssSelector, Using = ".prodtitle")]
+        private IWebElement productTitle;
+
+        [FindsBy(How = How.CssSelector, Using = ".currentprice")]
+        private IWebElement productPrice;
+
+        [FindsBy(How = How.CssSelector, Using = "input[type='submit']")]
+        private IWebElement addToCartButton;
+
+        [FindsBy(How = How.CssSelector, Using = ".alert.addtocart")]
+        private IWebElement productAddedAlert;
 
         public ProductPage(IWebDriver driver, WebDriverWait wait, Actions actions, MenuPage menu)
             : base(driver, wait, actions)
         {
             Menu = menu;
+            PageFactory.InitElements(driver, this);
         }
 
-        protected override ILog GetLogger()
+        public ProductPage AddProductXtimes(int timesToAddProduct)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < timesToAddProduct; i++)
+            {
+                AddProduct();
+            }
+            return this;
         }
 
-        protected override ProductPage GetThis()
+        public string GetProductName() => Regex.Replace(productTitle.Text, removeDashesRegex, "");
+
+        public decimal GetProductPrice()
         {
-            throw new NotImplementedException();
+            string productPriceString = Regex.Replace(productPrice.Text, "[$,]", "");
+            return Convert.ToDecimal(productPriceString);
         }
+
+        private void AddProduct()
+        {
+            ClickElementAndWaitToBeVisible(addToCartButton, nameof(addToCartButton), productAddedAlert, nameof(productAddedAlert))
+                .WaitForElementTextUpdate(Menu.NumberOfCartItems, ExpectedCartText(), nameof(Menu.NumberOfCartItems));
+        }
+
+        private string ExpectedCartText()
+        {
+            var currentCartProducts = Menu.GetNumberOfCartItems();
+            var expectedCartProducts = currentCartProducts + 1;
+            return expectedCartProducts.ToString();
+        }
+
+        protected override ILog GetLogger() => log;
+        protected override ProductPage GetThis() => this;
     }
 }
