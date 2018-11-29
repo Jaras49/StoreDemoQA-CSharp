f8f8b04aaa2ddca818b24d28f98477c28386f508
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-11-29 09:23:50 +0100

    CategoryPage impl

--- ShopCSharpSeleniumAutomation/page/category/CategoryPage.cs
+++ ShopCSharpSeleniumAutomation/page/category/CategoryPage.cs
@@ -1,34 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using log4net;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
 using OpenQA.Selenium.Support.UI;
+using ShopCSharpSeleniumAutomation.factory;
 
 namespace ShopCSharpSeleniumAutomation.page.category
 {
-    //TODO implement me
     public class CategoryPage : WebElementManipulator<CategoryPage>
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(CategoryPage).Name);
+        private static readonly string removeDashesRegex = "\\p{Pd}";
+
         public MenuPage Menu { get; }
+
+        [FindsBy(How = How.CssSelector, Using = "#content[role='main']")]
+        private IWebElement content;
+
+        [FindsBy(How = How.CssSelector, Using = "#content  h2 > a")]
+        private IList<IWebElement> products;
 
         public CategoryPage(IWebDriver driver, WebDriverWait wait, Actions actions, MenuPage menu)
             : base(driver, wait, actions)
         {
             Menu = menu;
+            PageFactory.InitElements(driver, this);
         }
 
-        protected override ILog GetLogger()
+        public ProductPage GoToRandomProductPageAndAssertItSwitchedCorrectly()
         {
-            throw new NotImplementedException();
+            var size = products.Count;
+            var random = new Random().Next(0, size);
+
+            IWebElement element = products.ElementAt(random);
+            string productName = Regex.Replace(element.Text, removeDashesRegex, "");
+
+            ClickElement(element, nameof(element));
+            ProductPage productPage = PageObjectFactory.CreateProductPage(driver);
+
+            AssertEquals(productName, productPage.GetProductName());
+            return productPage;
         }
 
-        protected override CategoryPage GetThis()
-        {
-            throw new NotImplementedException();
-        }
+        protected override ILog GetLogger() => log;
+        protected override CategoryPage GetThis() => this;
     }
 }
