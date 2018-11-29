f716efe875767992246f0b2de0c17214420fc6a9
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-11-29 10:44:30 +0100

    User impl

--- ShopCSharpSeleniumAutomation/model/User.cs
+++ ShopCSharpSeleniumAutomation/model/User.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace ShopCSharpSeleniumAutomation.model
 {
-    class User
+    public sealed class User
     {
+        private string Email { get; }
+        private string Firstname { get; }
+        private string Lastname { get; }
+        private string Address { get; }
+        private string City { get; }
+        private string State { get; }
+        private string Country { get; }
+        private string Phone { get; }
+
+        public User(string email, string firstname, string lastname, string address, string city, string state, string country, string phone)
+        {
+            Email = email;
+            Firstname = firstname;
+            Lastname = lastname;
+            Address = address;
+            City = city;
+            State = state;
+            Country = country;
+            Phone = phone;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var user = obj as User;
+            return user != null &&
+                   Email == user.Email &&
+                   Firstname == user.Firstname &&
+                   Lastname == user.Lastname &&
+                   Address == user.Address &&
+                   City == user.City &&
+                   State == user.State &&
+                   Country == user.Country &&
+                   Phone == user.Phone;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1063186798;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Email);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Firstname);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Lastname);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Address);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(City);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(State);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Country);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Phone);
+            return hashCode;
+        }
+
+        public override string ToString() => "User{" +
+                "email='" + Email + '\'' +
+                ", firstname='" + Firstname + '\'' +
+                ", lastname='" + Lastname + '\'' +
+                ", address='" + Address + '\'' +
+                ", city='" + City + '\'' +
+                ", state='" + State + '\'' +
+                ", country='" + Country + '\'' +
+                ", phone='" + Phone + '\'' +
+                '}';
     }
 }
