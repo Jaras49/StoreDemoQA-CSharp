95b077b6f7586eb8b378b52f61f4d2cae8808f91
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-11-30 12:10:16 +0100

    Order impl

--- ShopCSharpSeleniumAutomation/model/Order.cs
+++ ShopCSharpSeleniumAutomation/model/Order.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShopCSharpSeleniumAutomation.model
 {
-    class Order
+    public sealed class Order
     {
+        public IList<Product> Products { get; }
+        public decimal ShippingPrice { get; set; }
+
+        public Order(IList<Product> products)
+        {
+            Products = new List<Product>();
+            ShippingPrice = Decimal.Zero;
+            foreach (Product product in products)
+            {
+                AddProduct(product);
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            var products = IsInProducts(product.ProductName);
+            if (products.Any())
+            {
+                products.First().AddProduct();
+            }
+            else
+            {
+                Products.Add(product);
+            }
+        }
+
+        public decimal GetOrderPrice() => Products.Select(n => n.TotalPrice).Sum();
+
+        private IEnumerable<Product> IsInProducts(string name) => Products
+                .Select(n => n)
+                .Where(n => n.ProductName.Equals(name));
+
+        public override bool Equals(object obj)
+        {
+            var order = obj as Order;
+            return order != null &&
+                   EqualityComparer<IList<Product>>.Default.Equals(Products, order.Products) &&
+                   ShippingPrice == order.ShippingPrice;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 182108865;
+            hashCode = hashCode * -1521134295 + EqualityComparer<IList<Product>>.Default.GetHashCode(Products);
+            hashCode = hashCode * -1521134295 + ShippingPrice.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString() => "Order{" +
+                "products=" + Products +
+                ", shippingPrice=" + ShippingPrice +
+                '}';
     }
 }
