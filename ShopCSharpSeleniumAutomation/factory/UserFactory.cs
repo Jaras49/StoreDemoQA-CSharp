222e837ddf6b1ce81c6cb90b9715bd1b852a1443
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-11-30 15:00:26 +0100

    UserFactory impl

---
+++ ShopCSharpSeleniumAutomation/factory/UserFactory.cs
@@ -0,0 +1,35 @@
+﻿using ShopCSharpSeleniumAutomation.model;
+using System;
+using System.Linq;
+
+namespace ShopCSharpSeleniumAutomation.factory
+{
+    public sealed class UserFactory
+    {
+        private static readonly Random rand = new Random();
+        private static readonly string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static readonly string numbers = "0123456789";
+        private static readonly int lenght = 10;
+
+        private UserFactory() { }
+
+        public static User CreateRandomUser()
+        {
+            return new User
+                ($"{ GenerateRandomString() }@test.com",
+            $"firstname{ GenerateRandomString() }",
+            $"lastname{ GenerateRandomString() }",
+            $"address { GenerateRandomString() }",
+            $"city{ GenerateRandomString() }",
+            $"state{ GenerateRandomString() }",
+            "Poland",
+            GenerateRandomNumber());
+        }
+
+        private static string GenerateRandomString() => new string(Enumerable.Repeat(chars + numbers, lenght)
+                .Select(s => s[rand.Next(s.Length)]).ToArray());
+
+        private static string GenerateRandomNumber() => new string(Enumerable.Repeat(numbers, lenght)
+                .Select(s => s[rand.Next(s.Length)]).ToArray());
+    }
+}
