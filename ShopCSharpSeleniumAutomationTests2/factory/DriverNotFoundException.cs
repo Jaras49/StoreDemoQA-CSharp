689ea37cf1c00d0033e5d6fa2d3922d07a205d82
Jarosław Godwin <jgodwin@pl.sii.eu>
2018-12-03 16:43:46 +0100

    DriverFactory, DriverNotFoundException impl

---
+++ ShopCSharpSeleniumAutomationTests2/factory/DriverNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCSharpSeleniumAutomationTests.factory
+{
+    [Serializable]
+    public class DriverNotFoundException : Exception
+    {
+        public static readonly string noDriver = "No driver found for specified name";
+        public static readonly string noDriverType = "No driver found for specified DRIVER TYPE ";
+
+        public DriverNotFoundException(string message) : base(message) { }
+    }
+}
